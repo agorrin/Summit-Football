8119f05648a8c7a367bc8f7d6d9cb1b7bb13f60a
Andrew Gorringe <andrewbrian.g@gmail.com>
2015-06-06 12:18:22 -0600

    updated

---
+++ app.js
@@ -0,0 +1,43 @@
+var app = angular.module('summitFootball', ['ngRoute', 'firebase', 'routeStyles'])
+.config(function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: './pages/main/main.html',
+        controller: 'mainCtrl',
+        css: '../css/mainstyles.css'
+      }).
+      when('/staff', {
+        templateUrl: './pages/staff/staff.html',
+        controller: 'staffCtrl',
+        css: '../css/staffstyles.css'
+      }).
+       when('/schedule', {
+        templateUrl: './pages/schedule/schedule.html',
+        controller: 'scheduleCtrl',
+        css: '../css/schedulestyles.css'
+      }).
+      when('/contact', {
+        templateUrl: './pages/contact/contact.html',
+        controller: 'contactCtrl',
+        css: '../css/contactstyles.css'
+      }).
+      when('/roster', {
+        templateUrl: './pages/roster/roster.html',
+        controller: 'rosterCtrl',
+        css: '../css/rosterstyles.css'
+      }).
+      when('/login', {
+        templateUrl: './pages/login/login.html',
+        controller: 'loginCtrl',
+        css: '../css/loginstyles.css'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+
+  });
+
+app.constant('fb', {
+  url: 'https://sahs-roster.firebaseio.com/'
+});
+
